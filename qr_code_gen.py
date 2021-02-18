36ceaf62f86412150d3d47319af7b4f81b910182
Prince Raj <prince.raj29042004@gmail.com>
2021-02-18 16:44:27 +0530

    Fixed some errors...

--- qr_code_gen.py
+++ qr_code_gen.py
@@ -3,12 +3,16 @@ from tkinter import *
 from PIL import ImageTk, Image
 import time
 
-root = Tk()
-canvas = Canvas(root, width=400, height=400)
-canvas.pack()
-data = input("Enter the link/data:")
-code = pyqrcode.create(data)
-img = code.png(f'qrcode{round(time.time())}.png', scale=8)
-img_to_display = ImageTk.PhotoImage(Image.open(f"qrcode{round(time.time())}.png"))
-canvas.create_image(40, 40, anchor=NW, image=img_to_display)
-root.mainloop()
+def qr_code(data):
+    root = Tk()
+    canvas = Canvas(root, width=400, height=400)
+    canvas.pack()
+    code = pyqrcode.create(data)
+    code.png(f'qrcode_{time.strftime("%Y%m%d")}.png', scale=8)
+    img_to_display = ImageTk.PhotoImage(Image.open(f'qrcode_{time.strftime("%Y%m%d")}.png'))
+    canvas.create_image(40, 40, anchor=NW, image=img_to_display)
+    root.mainloop()
+
+if __name__ == "__main__":
+    data = input("Enter the link:") # Or simply put the data here
+    qr_code(data)
